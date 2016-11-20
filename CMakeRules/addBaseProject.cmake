0137eb4dbaaa16ebb6a9c3fa32d3bd76abbd9ad4
Ryan Honeyager <ryan@honeyager.info>
2016-11-20 15:21:13 -0500

    CMake warnings fixed

--- CMakeRules/addBaseProject.cmake
+++ CMakeRules/addBaseProject.cmake
@@ -16,16 +16,16 @@ endmacro(getMSVCappend)
 macro(addBaseProject)
 
 # Compiler detection and rules
-if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+if (${CMAKE_CXX_COMPILER_ID} MATCHES "Clang")
 	SET (CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} "-std=c++11")
   # using Clang
-elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+elseif (${CMAKE_CXX_COMPILER_ID} MATCHES "GNU")
   # using GCC
 	SET (CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} "-std=c++11 -fPIC")
-elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")
+elseif (${CMAKE_CXX_COMPILER_ID} MATCHES "Intel")
   # using Intel C++
   SET (CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} "-std=c++11")
-elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
+elseif (${CMAKE_CXX_COMPILER_ID} MATCHES "MSVC")
   # using Visual Studio C++
   SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /MP")
 endif()
