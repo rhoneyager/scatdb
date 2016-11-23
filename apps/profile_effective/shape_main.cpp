f0e774b4d72b64d48579ac14714d1abbfbaafa79
Ryan Honeyager <ryan@honeyager.info>
2016-11-23 00:27:50 -0500

    Stopping for tonight.

--- apps/profile_effective/shape_main.cpp
+++ apps/profile_effective/shape_main.cpp
@@ -108,19 +108,21 @@ int main(int argc, char** argv) {
 			if (!ds) ds = 40.f;
 			bints(COL_ID) = shp->hash()->lower;
 			bints(COL_NUM_LATTICE) = (uint64_t) shp->numPoints();
-			bfloats(COL_AEFF) = ds * std::pow(3.f*((float)shp->numPoints()) / (4.f*3.141592654f), 1.f / 3.f);
+			bfloats(COL_AEFF) = ds * std::pow(3.f*((float)shp->numPoints()) / (4.f*3.141592654f), 1.f / 3.f); // in um
 			
 			for (int axis = 0; axis < 3; ++axis) {
 				auto shpproj = shape::algorithms::projectShape(shp, axis);
 				float pmd = 0, parea = 0, pcaf = 0;
 				shape::algorithms::getProjectedStats(shpproj, pmd, parea, pcaf);
 
-				if ((ds * pmd) > bfloats(COL_MD)) bfloats(COL_MD) = ds * pmd;
-				bfloats(COL_PROJAREA) += ds * (1.f / 3.f) * parea;
-				bfloats(COL_CIRCUMAREAFRAC) += (1.f / 3.f) * pcaf;
+				if ((ds * pmd / 1000.f) > bfloats(COL_MD)) bfloats(COL_MD) = ds * pmd / 1000.f; // in mm
+				bfloats(COL_PROJAREA) += ds * ds * (1.f / 3.f) * parea / 1000.f; // in mm^2
+				bfloats(COL_CIRCUMAREAFRAC) += (1.f / 3.f) * pcaf; // dimensionless
 			}
 			// Fall velocity is calculated directly from the final projected area.
-			bfloats(COL_FALLVEL);
+
+			SDBR_log("scatdb_profile_shape", logging::CRITICAL, "Fall velocity calculation not yet implemented.");
+			bfloats(COL_FALLVEL) = 0; // TODO. In m/s.
 			++i;
 		}                                       
 		// Write the raw tables
@@ -132,14 +134,68 @@ int main(int argc, char** argv) {
 		std::shared_ptr<H5::H5File> file;
 		boost::filesystem::path p(sout);
 		file = std::shared_ptr<H5File>(new H5File(sout, H5F_ACC_TRUNC));
-		auto base = scatdb::plugins::hdf5::openOrCreateGroup(file, "output");
-		auto fsbase = scatdb::plugins::hdf5::openOrCreateGroup(base, "shape");
+		auto base = scatdb::plugins::hdf5::openOrCreateGroup(file, "output-shape-falls");
+		auto fsbase = scatdb::plugins::hdf5::openOrCreateGroup(base, "per-shape");
+		plugins::hdf5::addDatasetEigen(fsbase, "ints", sints);
+		plugins::hdf5::addDatasetEigen(fsbase, "floats", sfloats);
 
+		auto fprof = scatdb::plugins::hdf5::openOrCreateGroup(file, "per-profile");
 
 		// Iterate over the profiles
+		int i = 0;
+		for (const auto &prof : *(allprofiles.get()))
 		{
-			// Perform the binning, and write to the binned tables
+			string profid("profile_");
+			profid.append(boost::lexical_cast<string>(i));
+			auto fpro = scatdb::plugins::hdf5::openOrCreateGroup(fprof, profid.c_str());
+			auto fraw = scatdb::plugins::hdf5::openOrCreateGroup(fpro, "Raw");
+			prof->writeHDF5File(fraw, profid.c_str());
 
+			auto pts = prof->getParticleTypes();
+			string sproftype(scatdb::profiles::defs::stringify(pts));
+			
+			// Bin the scattering database according to the profile boundaries
+			auto data = prof->getData();
+			vector < shared_ptr<const db> > binned_raw;
+			vector<shared_ptr<const db::data_stats> > binned_stats;
+			Eigen::Matrix<float, Eigen::Dynamic, 1> parInts;
+			Eigen::Matrix<uint64_t, Eigen::Dynamic, 1> parCounts;
+			parInts.resize(data->rows(), 1);
+			parInts.setZero();
+			parCounts.resize(data->rows(), 1);
+			parCounts.setZero();
+			auto fpartials = scatdb::plugins::hdf5::openOrCreateGroup(fpro, "Bins");
+			const int w = ((int)log10(data->rows())) + 1;
+			for (int row = 0; row < data->rows(); ++row) {
+				string strbin;
+				{
+					ostringstream sstrbin;
+					sstrbin << setfill('0') << setw(w) << row;
+					strbin = sstrbin.str();
+				}
+				auto obin = scatdb::plugins::hdf5::openOrCreateGroup(fpartials, strbin.c_str());
+				float binMin = (*data)(row, scatdb::profiles::defs::BIN_LOWER) / 1000.f; // mm
+				float binMax = (*data)(row, scatdb::profiles::defs::BIN_UPPER) / 1000.f; // mm
+				float binMid = (*data)(row, scatdb::profiles::defs::BIN_MID) / 1000.f; // mm
+				float binWidth = binMax - binMin;
+				float binConc = (*data)(row, scatdb::profiles::defs::CONCENTRATION); // m^-4
+				
+				
+				// Take the shapes and filter them according to max dimension. TODO!!!!!!!!!!!!!!!!!!!!
+				fbin->addFilterFloat(db::data_entries::SDBR_MAX_DIMENSION_MM, binMin, binMax);
+				auto dbin = fbin->apply(db_ros_f_sorted);
+				binned_raw.push_back(dbin);
+				auto sbin = dbin->getStats();
+				binned_stats.push_back(sbin);
+				auto odbin = scatdb::plugins::hdf5::openOrCreateGroup(obin, "Filtered");
+				auto osbin = scatdb::plugins::hdf5::openOrCreateGroup(obin, "Stats");
+				scatdb::plugins::hdf5::addAttr<float>(obin, "bin_min_mm", binMin);
+				scatdb::plugins::hdf5::addAttr<float>(obin, "bin_max_mm", binMax);
+				scatdb::plugins::hdf5::addAttr<float>(obin, "bin_mid_mm", binMid);
+				scatdb::plugins::hdf5::addAttr<float>(obin, "bin_width_mm", binWidth);
+				scatdb::plugins::hdf5::addAttr<float>(obin, "bin_conc_m^-4", binConc);
+				float medCbk = sbin->floatStats(db::data_entries::SDBR_MEDIAN, db::data_entries::SDBR_CBK_M);
+			}
 			// Calculate the bulk and mass-weighted quantities. Write these also.
 		}
 
